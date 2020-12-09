184b6e926dc04f3446ff694058da01882df9d029
khramov <khramov.dev@gmail.com>
2020-12-09 19:58:52 +0300

    UI Prototype and skeleton added

---
+++ Calc/Model.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+struct Calculations {
+    
+    private enum Functions {
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case reset
+        case equal
+    }
+    
+    private var operation: Dictionary<String, Functions> = [
+        "±" : Functions.unaryOperation({ -$0 }),
+        //"%" : Functions.unaryOperation( { $0 / 100}),
+        //"," : Functions.unaryOperation,
+        "×" : Functions.binaryOperation({ $0 * $1 }),
+        "÷" : Functions.binaryOperation({ $0 / $1}),
+        "-" : Functions.binaryOperation({ $0 - $1}),
+        "+" : Functions.binaryOperation({ $0 + $1}),
+        "=" : Functions.equal,
+        "AC" : Functions.reset
+    ]
+    
+    
+    
+    
+    
+    
+}
--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -1,19 +1,37 @@
-//
-//  ViewController.swift
-//  Calc
-//
-//  Created by Владимир Храмов on 09.12.2020.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var userIsTyping: Bool = false
+    var firstOperand: String?
+    
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func numberButtonTouched(_ sender: UIButton) {
+        if userIsTyping {
+            resultLabel.text! += sender.titleLabel!.text!
+        }
+        else {
+            resultLabel.text! = ""
+            resultLabel.text! += sender.titleLabel!.text!
+        }
+        userIsTyping = true
+        //firstOperand += sender.titleLabel!.text
+        
+    }
+    
+    @IBAction func functionalButtonTouched(_ sender: UIButton) {
+        userIsTyping = false
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
